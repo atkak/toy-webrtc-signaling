15daa1ba5915c64be8cb64e05892508acd6ed40b
atkak <pasapasa.va1104@gmail.com>
2021-05-07 20:09:23 +0900

    reorganize into join module

--- src/signaling.rs
+++ src/signaling.rs
@@ -1,7 +1,10 @@
 use actix::{prelude::*, Actor, Addr, AsyncContext, StreamHandler, Supervised};
 use actix_web_actors::ws;
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::Value;
+
+use self::join::*;
+
+mod join;
 
 pub struct ClientSession {}
 
@@ -15,64 +18,13 @@ impl Actor for ClientSession {
     type Context = ws::WebsocketContext<Self>;
 }
 
-impl Handler<RoomJoinMessage> for ClientSession {
-    type Result = ();
-
-    fn handle(&mut self, msg: RoomJoinMessage, ctx: &mut Self::Context) {
-        match msg {
-            RoomJoinMessage::Joined { username } => {
-                let joined = JoinedEvent {
-                    username: username.clone(),
-                };
-                let joined_value = json!( {
-                    "event": "joined",
-                    "from": username,
-                    "body": joined,
-                });
-                let json = serde_json::to_string(&joined_value).unwrap();
-
-                ctx.text(json);
-            }
-            RoomJoinMessage::Full => {
-                let joined_value = json!( {
-                    "event": "room_full",
-                });
-                let json = serde_json::to_string(&joined_value).unwrap();
-
-                ctx.text(json);
-            }
-            RoomJoinMessage::AlreadyJoined => {
-                let joined_value = json!( {
-                    "event": "room_full",
-                });
-                let json = serde_json::to_string(&joined_value).unwrap();
-
-                ctx.text(json);
-            }
-            _ => (),
-        }
-    }
-}
-
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ClientSession {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Text(text)) => {
                 let value: Value = serde_json::from_str(&text).unwrap();
                 match &value["event"] {
-                    Value::String(event) if event == "join" => {
-                        let body = value["body"].clone();
-                        let join: JoinEvent = serde_json::from_value(body).unwrap();
-                        println!("join from: {}", join.username);
-
-                        let msg = RoomJoinMessage::Join {
-                            username: join.username,
-                            addr: ctx.address(),
-                        };
-
-                        let room_addr = Room::from_registry();
-                        room_addr.do_send(msg);
-                    }
+                    Value::String(event) if event == "join" => handle_join(value, ctx.address()),
                     Value::String(event) if event == "offer" => {
                         let value: Value = serde_json::from_str(&text).unwrap();
                         println!("offer from: {}", value["from"].as_str().unwrap());
@@ -91,30 +43,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ClientSession {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-struct JoinEvent {
-    username: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct JoinedEvent {
-    username: String,
-}
-
-#[derive(Message)]
-#[rtype(result = "()")]
-enum RoomJoinMessage {
-    Join {
-        username: String,
-        addr: Addr<ClientSession>,
-    },
-    Joined {
-        username: String,
-    },
-    Full,
-    AlreadyJoined,
-}
-
 #[derive(Default)]
 struct Room {
     members: Vec<Addr<ClientSession>>,
@@ -126,36 +54,3 @@ impl Actor for Room {
 
 impl Supervised for Room {}
 impl SystemService for Room {}
-
-impl Handler<RoomJoinMessage> for Room {
-    type Result = ();
-
-    fn handle(&mut self, msg: RoomJoinMessage, _: &mut Self::Context) {
-        match msg {
-            RoomJoinMessage::Join { username, addr } => {
-                let addr_ref = &addr;
-
-                if !self.members.contains(addr_ref) && self.members.len() > 2 {
-                    addr.do_send(RoomJoinMessage::Full);
-                    return;
-                }
-
-                if self.members.contains(addr_ref) {
-                    addr.do_send(RoomJoinMessage::AlreadyJoined);
-                    return;
-                }
-
-                self.members.push(addr.clone());
-
-                if self.members.len() <= 1 {
-                    return;
-                }
-
-                let remote_addr = self.members.iter().find(|&e| e != addr_ref).unwrap();
-
-                remote_addr.do_send(RoomJoinMessage::Joined { username });
-            }
-            _ => (),
-        }
-    }
-}
---
+++ src/signaling/join.rs
@@ -0,0 +1,115 @@
+use actix::{prelude::*, Addr};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+use super::{ClientSession, Room};
+
+pub fn handle_join(value: Value, addr: Addr<ClientSession>) {
+    let body = value["body"].clone();
+    let join: JoinEvent = serde_json::from_value(body).unwrap();
+    println!("join from: {}", join.username);
+
+    let msg = RoomJoinMessage::Join {
+        username: join.username,
+        addr,
+    };
+
+    let room_addr = Room::from_registry();
+    room_addr.do_send(msg);
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+enum RoomJoinMessage {
+    Join {
+        username: String,
+        addr: Addr<ClientSession>,
+    },
+    Joined {
+        username: String,
+    },
+    Full,
+    AlreadyJoined,
+}
+
+#[derive(Serialize, Deserialize)]
+struct JoinEvent {
+    username: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct JoinedEvent {
+    username: String,
+}
+
+impl Handler<RoomJoinMessage> for ClientSession {
+    type Result = ();
+
+    fn handle(&mut self, msg: RoomJoinMessage, ctx: &mut Self::Context) {
+        match msg {
+            RoomJoinMessage::Joined { username } => {
+                let joined = JoinedEvent {
+                    username: username.clone(),
+                };
+                let joined_value = json!( {
+                    "event": "joined",
+                    "from": username,
+                    "body": joined,
+                });
+                let json = serde_json::to_string(&joined_value).unwrap();
+
+                ctx.text(json);
+            }
+            RoomJoinMessage::Full => {
+                let joined_value = json!( {
+                    "event": "room_full",
+                });
+                let json = serde_json::to_string(&joined_value).unwrap();
+
+                ctx.text(json);
+            }
+            RoomJoinMessage::AlreadyJoined => {
+                let joined_value = json!( {
+                    "event": "room_full",
+                });
+                let json = serde_json::to_string(&joined_value).unwrap();
+
+                ctx.text(json);
+            }
+            _ => (),
+        }
+    }
+}
+
+impl Handler<RoomJoinMessage> for Room {
+    type Result = ();
+
+    fn handle(&mut self, msg: RoomJoinMessage, _: &mut Self::Context) {
+        match msg {
+            RoomJoinMessage::Join { username, addr } => {
+                let addr_ref = &addr;
+
+                if !self.members.contains(addr_ref) && self.members.len() > 2 {
+                    addr.do_send(RoomJoinMessage::Full);
+                    return;
+                }
+
+                if self.members.contains(addr_ref) {
+                    addr.do_send(RoomJoinMessage::AlreadyJoined);
+                    return;
+                }
+
+                self.members.push(addr.clone());
+
+                if self.members.len() <= 1 {
+                    return;
+                }
+
+                let remote_addr = self.members.iter().find(|&e| e != addr_ref).unwrap();
+
+                remote_addr.do_send(RoomJoinMessage::Joined { username });
+            }
+            _ => (),
+        }
+    }
+}
